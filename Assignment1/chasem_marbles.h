beb223e57bcf08dd43f0847af65a47379fedbab9
chasem <chasem@oregonstate.edu>
2023-04-10 14:41:12 -0700

    Created driver file and header file. Added a Marble class after much research. Still need to implement Jar.

---
+++ Assignment1/chasem_marbles.h
@@ -0,0 +1,36 @@
+#ifndef CHASEM_MARBLES_H
+#define CHASEM_MARBLES_H
+
+#endif //CHASEM_MARBLES_H
+
+#include <iostream>
+#include <string>
+#include <vector> // contiguous memory structure
+
+using namespace std;
+
+class Marble {
+
+public:
+    // declaration and initialization follows same order
+    Marble(const string& color, const string& style, float size)  // string passed by reference like assembly arrays
+        : _color(color), _style(style), _size(size) {}
+
+    //public getters
+    string getColor() const { return _color; }
+    string getStyle() const { return _style; }
+    float getSize() const { return _size; }
+
+private:
+    string _color;
+    string _style;
+    float _size;
+
+};
+
+class Jar {
+
+public:
+
+
+};
