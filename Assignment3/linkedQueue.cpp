dbfef74d7d5038bcecc140094ad405af9acbbb74
chasem <chasem@oregonstate.edu>
2023-04-24 12:12:40 -0700

    created assignment 3 directory

--- Assignment3/linkedQueue.cpp
+++ Assignment3/linkedQueue.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+class Queue {
+    Queue(){
+        top = nullptr;
+        bottom = nullptr;
+    }
+
+    private
+};
+
+int main() {
+    Queue myQueue;
+
+    cout << "myQueue.peek(): " << myQueue.peek() << endl;
+    cout << "myQueue.pop(): " << myQueue.pop() << endl;
+
+    myQueue.push(42)
+}
