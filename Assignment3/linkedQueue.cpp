e24eea37ad84bef6893b0459f62a9dd30d5a4798
chasem <chasem@oregonstate.edu>
2023-04-24 13:10:55 -0700

    added push function that checks for empty list

--- Assignment3/linkedQueue.cpp
+++ Assignment3/linkedQueue.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr){}
+};
+
 class Queue {
-    Queue(){
-        top = nullptr;
-        bottom = nullptr;
+private:
+    Node* front_;
+    Node* rear_;
+
+public:
+    Queue() : front_(nullptr), rear_(nullptr) {}
+
+    void push(int newData) {
+        Node *newNode = new Node(newData);
+        if (rear_ = nullptr){
+            rear_ = newNode;
+            front_ = newNode;
+            return;
+        }
+        rear_->next = newNode;
+        rear_ = newNode;
     }
 
-    private
 };
 
 int main() {
